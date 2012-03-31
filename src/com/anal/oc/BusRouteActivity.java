254d4a176803833629991105237f9f8e84753798
Andrew <andrew@Andrews-MacBook-Pro.local>
2012-03-31 19:48:00 -0400

    map and route search

--- src/com/anal/oc/BusRouteActivity.java
+++ src/com/anal/oc/BusRouteActivity.java
@@ -2,16 +2,27 @@ package com.anal.oc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class BusRouteActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        TextView textview = new TextView(this);
-        textview.setText("This is the Artists tab");
-        setContentView(textview);
-    }
+		setContentView(R.layout.busroutesearch);
+
+		final Button search = (Button) findViewById(R.id.routeSearch);
+		search.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				setContentView(R.layout.busroute);
+			}
+		});     
+	}
+
+	public void close(View v) {
+		setContentView(R.layout.busroutesearch);
+	}
 }
--- src/com/anal/oc/BusStopActivity.java
+++ src/com/anal/oc/BusStopActivity.java
@@ -2,14 +2,25 @@ package com.anal.oc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class BusStopActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.busstopsearch);
-    }
+		setContentView(R.layout.busstopsearch);
+
+		final Button search = (Button) findViewById(R.id.stopSearch);
+		search.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				setContentView(R.layout.busstopsearch);
+			}
+		});        
+	}
 }
