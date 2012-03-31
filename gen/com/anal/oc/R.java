97f46ee504261f3686a0cb048707ad54da1f257c
Andrew <andrew@Andrews-MacBook-Pro.local>
2012-03-31 16:07:38 -0400

    favourite activity java code

--- gen/com/anal/oc/R.java
+++ gen/com/anal/oc/R.java
@@ -13,8 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int entireFavourite=0x7f050000;
+        public static final int remove=0x7f050005;
+        public static final int stopName=0x7f050002;
+        public static final int stopRoute=0x7f050004;
+        public static final int stopTime=0x7f050007;
+        public static final int tableRow1=0x7f050001;
+        public static final int tableRow2=0x7f050003;
+        public static final int tableRow3=0x7f050006;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int favourites=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/anal/oc/FavoritesActivity.java
+++ src/com/anal/oc/FavoritesActivity.java
@@ -2,16 +2,26 @@ package com.anal.oc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TableLayout;
 
 public class FavoritesActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView textview = new TextView(this);
-        textview.setText("This is the Artists tab");
-        setContentView(textview);
+        setContentView(R.layout.favourites);
+        
+        final Button remove = (Button) findViewById(R.id.remove);
+        remove.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            	TableLayout favourite = (TableLayout) findViewById(R.id.entireFavourite);
+            	favourite.setVisibility(View.GONE);
+            }
+        });
     }
+    
 }
